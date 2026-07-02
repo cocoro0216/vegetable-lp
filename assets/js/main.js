13ea2e8c82e96f3de5ff6efa70fe553817bc7a27
cocoro0216 <cocoronnnnny@gmail.com>
2026-07-02 13:00:57 +0900

    アニメーションの下準備

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,44 @@
 'use strict';
-{
+
+// =================================================
+// gsap
+// =================================================
+gsap.registerPlugin(ScrollTrigger)
+
+// html,css書いてから続きの設定をする（MVでキャッチコピーがフェードインとCTAでボタンがふわっと表示
+
+// MV
+
+// CTA
+
+
+
+
+
+
+
+
+// =================================================
+// jquery
+// =================================================
     $(function(){
-        // sp header_btn & menu
-        $('.header_btn').on('click', function(){
-            $('.nav').toggleClass('active');
-        });
-
-        $('.nav__btn').on('click', function(){
-            $('.nav').removeClass('active');
-        });
-
-
-
+        // sp　ハンバーガーメニュー５で実装
+    $('.btn-trigger').on('click', function() {
+            $(this).toggleClass('active');
+        return false;
+  });
     });
-    // jquery
 
 
-}
+    // アコーディオンの設定
+    $(function(){
+  $('.accordion_one .ac_header').click(function(){
+    $(this).next('.ac_inner').slideToggle();
+    $(this).toggleClass("open");
+  });
+});
+
+
+
+
 
